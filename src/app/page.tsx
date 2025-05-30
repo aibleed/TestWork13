c8eac940672ff4d19de2e086512a638a2222585d
Dzmitry K. <gawrfan@gmail.com>
2025-05-30 07:40:08 +0300

    refactor: root layout, page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-import styles from "./page.module.css";
+import { Suspense } from "react";
+import { fetchProducts } from "./actions/products";
+import Container from "./components/Container/Container";
+import ProductsList from "./components/ProductsList/ProductsList";
+import styles from "./page.module.scss";
+import { verifySession } from "./lib/dal";
 
-export default function Home() {
+export default async function Home() {
+  const products = fetchProducts();
+  const session = await verifySession();
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        2
-      </main>
-    </div>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Products</h1>
+      <Container>
+        <Suspense fallback={<div className={styles.loading}>Loading...</div>}>
+          <ProductsList
+            isCartButton={session?.isAuth ?? false}
+            products={products}
+          />
+        </Suspense>
+      </Container>
+    </main>
   );
 }
